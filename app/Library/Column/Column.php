5b64585e91acd9cfba475cd2eddb9c0ae04e1b33
bright-coder <k.kanchalew.b@gmail.com>
2017-09-05 19:52:29 +0700

    edit col & table class

--- app/Library/Column/Column.php
+++ app/Library/Column/Column.php
@@ -3,7 +3,7 @@
 class Column{
     
     private $name,$datatype,$isNull,$defaultValue,$numRow;
-    private $constraint = ['FK' => NULL,'Unique' => NULL,'Check' => NULL];
+    private $constraint = ['Unique' => NULL,'Check' => NULL];
 
     public function construct(SchemaDB $info){
 
--- app/Library/Table/Table.php
+++ app/Library/Table/Table.php
@@ -3,10 +3,31 @@
 namespace App\Library\Table;
 
 class Table{
-    private $columns = [];
-    private $pkColumns = [];
+    private $name;
+    private $columns = NULL;
+    private $pkColumns = NULL;
+    private $fkColumns = NULL;
 
-    public function __construct(){
-        
+    public function __construct(string $name){
+        $this->name = $name;
     }
+
+    public function addColumn(Column $col): void{
+        if($this->columns === NULL) $columns = [];
+        $this->columns[$col->getName()] = $col;
+    }
+
+    public function addColumns(array $cols = []): void{
+        if($this->columns === NULL) $columns = [];
+        $this->columns = array_merge($this->columns,$cols);
+    }
+
+    public function getColumns(): array{
+        return $this->columns;
+    }
+
+    private function addPkcolumn(array $pkCols){
+    }
+
+
 }
