8efe9dbac85cfb95637e4085e9c17b8e0ce04218
bright-coder <k.kanchalew.b@gmail.com>
2017-09-19 19:23:30 +0700

    add method in class Table & Column

--- app/Library/Column/Column.php
+++ app/Library/Column/Column.php
@@ -1,20 +1,50 @@
 <?php
 
+namespace App\Library\Column;
+
 class Column{
     
-    private $name,$datatype,$isNull,$defaultValue,$numRow;
-    private $constraint = ['Unique' => NULL,'Check' => NULL
-        ,'isFK' => FALSE ,'isPK' => FALSE ];
+    private $name,$datatype,$isNullable = FALSE,$default = NULL,$numRow;
+    private $constraint = ['Unique' => NULL,'Check' => NULL];
 
     public function construct(SchemaDB $info){
 
     }
 
-    public function getName(){
+    public function getName(): string{
         return $this->name;
     }
 
-    public function get
+    public function isUnique(): boolean{
+        return $this->constraint['Unique'] === NULL ? FALSE : TRUE;
+    }
 
+    public function getUnique(): Unique{
+        return $this->constraint['Unique'];
+    }
+
+    public function setUnique(Unique $unique): void{
+        $this->constraint['Unique'] = $unique;
+    }
+
+    public function isCheck(): boolean{
+        return $this->constraint['Check'] === NULL ? FALSE : TRUE;
+    }
+
+    public function getCheck(): Check{
+        return $this->constraint['Check']; 
+    }
+
+    public function setCheck(Check $check): void{
+        $this->constraint['Check'] = $check;
+    }
+
+    public function isNullable(): boolean{
+        return $this->isNullable;
+    }
+
+    public function getDefault(): string{
+        return $this->default;
+    }
 
 }
--- app/Library/Table/Table.php
+++ app/Library/Table/Table.php
@@ -4,30 +4,45 @@ namespace App\Library\Table;
 
 class Table{
     private $name;
-    private $columns = NULL;
-    private $pkColumns = NULL;
-    private $fkColumns = NULL;
+    private $columns;
+    private $pk = NULL;
+    private $fk = NULL;
 
     public function __construct(string $name){
         $this->name = $name;
+        $this->columns = [];
     }
 
     public function addColumn(Column $col): void{
-        if($this->columns === NULL) $columns = [];
         $this->columns[$col->getName()] = $col;
     }
 
     public function addColumns(array $cols = []): void{
-        if($this->columns === NULL) $columns = [];
         $this->columns = array_merge($this->columns,$cols);
     }
 
-    public function getColumns(): array{
+    public function getAllColumns(): array{
         return $this->columns;
+    }    
+    
+    public function getColumnByName(string $colName){
+        return $this->columns[$colName];
     }
 
-    private function addPkcolumn(array $pkCols): void{
+    public function setPK(PrimaryKey $pk): void{
+        $this->pk = $pk;
     }
 
+    public function getPK(): PrimaryKey{
+        return $this->pk;
+    }
+
+    public function setFK(ForeignKey $fk): void{
+        $this->fk = $fk;
+    }
+
+    public function getFK(): ForeignKey{
+        return $this->fk;
+    }
 
 }
