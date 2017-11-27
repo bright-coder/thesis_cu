f5eb7764cf3698b00b134ebd0413850397f421d9
bright-coder <k.kanchalew.b@gmail.com>
2017-11-27 21:14:30 +0700

    add functional Requirment class

---
+++ app/Library/FunctionalRequirement/FR.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Library\FunctionalRequirement;
+
+class FR
+{
+
+    private $id;
+
+    private $no;
+
+    private $description;
+
+    private $version;
+
+    private $frInputs;
+
+    public function __construct()
+    {
+        $this->frInputs = [];
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getNo(): string
+    {
+        return $this->$no;
+    }
+
+    public function setNo(string $no): void
+    {
+        $this->$no = $no;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+
+    public function setVersion(string $version): void
+    {
+        $this->version = $version;
+    }
+
+    public function addInput(FRInput $frInput): void
+    {
+        $this->frInputs[$frInput->getId()] = $frInput;
+    }
+
+    public function getAllInputs(): array
+    {
+        return $this->frInputs;
+    }
+
+    public function getInputById(string $id): FRInput
+    {
+        return $this->frInputs[$id];
+    }
+
+}
---
+++ app/Library/FunctionalRequirement/FRInput.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Library\FunctionalRequirement;
+
+class FRInput
+{
+
+    private $id;
+    private $name;
+    private $dataType;
+    private $default;
+    private $isNullable;
+    private $isUnique;
+    private $min = null;
+    private $max = null;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDataType(): DataType
+    {
+        return $this->dataType;
+    }
+
+    public function setDataType(DataType $dataType): void
+    {
+        $this->dataTye = $dataType;
+    }
+
+    public function getDefault(): string
+    {
+        return $this->default;
+    }
+
+    public function setDefault(string $default): void
+    {
+        $this->default = $default;
+    }
+
+    public function isNullable(): bool
+    {
+        return $this->isNullable;
+    }
+
+    public function setNullable(\boolean $isNullable): void
+    {
+        $this->isNullable = $isNullable;
+    }
+
+    public function isUnique(): bool
+    {
+        return $this->isUnique;
+    }
+
+    public function getMin(): string
+    {
+        return $this->$min;
+    }
+
+    public function setMin(string $min): void
+    {
+        $this->min = $min;
+    }
+
+    public function getMax(): string
+    {
+        return $this->$max;
+    }
+
+    public function setMax(string $max): void
+    {
+        $this->max = $max;
+    }
+
+}
