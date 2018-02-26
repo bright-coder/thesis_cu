815a87154bbec503f7921c6fcf2bf3574f56b218
bright-coder <k.kanchalew.b@gmail.com>
2018-02-26 13:35:56 +0700

    60% modified analysisDB impact

--- app/Library/State/AnalyzeImpactDBState.php
+++ app/Library/State/AnalyzeImpactDBState.php
@@ -36,7 +36,7 @@ class AnalyzeImpactDBState implements StateInterface
         } else {
             $this->getDbSchema();
             if ($this->saveDbSchema($changeAnalysis->getProjectId())) {
-                $this->analysis($requestInfo['functionalRequirements'],$requestInfo['changeRequest']);
+                $this->analysis($requestInfo['functionalRequirements'], $requestInfo['changeRequest']);
             } else {
                 $changeAnalysis->setMessage($this->message);
                 $changeAnalysis->setStatusCode(303);
@@ -170,18 +170,147 @@ class AnalyzeImpactDBState implements StateInterface
         return true;
     }
 
-    private function analysis(array $functionalRequirements, array $changeRequest): void {
+    private function analysis(array $functionalRequirements, array $changeRequest): void
+    {
 
-        foreach ($functionalRequirements as $functionalRequirement) {
-                if($functionalRequirement['no'] == $changeRequest['functionalRequirementNo']) {
-                    $changeFunctionRequirement = $functionalRequirement;
-                    break;
-                }
-        }
+        $changeFunctionRequirement = $this->findFrByNo($changeRequest['functionalRequirementNo'], $functionalRequirements);
 
         foreach ($changeRequest['inputs'] as $changeInput) {
-            
+
+            if ($changeInput['changeType'] == "edit") {
+                $impact = $this->analysisEditing($changeInput, $this->findInputByName($changeInput['name'], $changeFunctionRequirement));
+
+            } elseif ($changeInput['changeType'] == "add") {
+                $impact = $this->analysisAdding($changeInput, $this->findInputByName($changeInput['name'], $changeFunctionRequirement));
+            } else {
+                $impact = $this->analysisDeleting($changeInput, $this->findInputByName($changeInput['name'], $changeFunctionRequirement));
+            }
         }
+
+    }
+
+    private function findFrByNo(string $no, array $functionalRequirements): array
+    {
+        foreach ($functionalRequirements as $functionalRequirement) {
+            if ($functionalRequirement['no'] == $changeRequest['functionalRequirementNo']) {
+                return $functionalRequirement;
+                break;
+            }
+        }
+        return [];
+    }
+
+    private function findInputByName(string $name, array $functionalRequirementInputs): array
+    {
+        foreach ($functionalRequirementInputs as $input) {
+            if ($input['name'] == $name) {
+                return $input;
+            }
+        }
+        return [];
+    }
+
+    private function analysisEditing(array $changeInput, array $frInput): bool
+    {
+        $dbColumn = $this->dbTarget->getTableByName($frInput['tableName'])->getColumnByName($frInput['columnName']);
+        if ($changeInput['dataType'] !== null) {
+           if($this->findInstanceImpactByDataType($changeInput['dataType'],$dbColumn->getDataType()->getType()) ) {
+               return true;
+           }
+        }
+        if ($changeInput['length'] !== null) {
+            $dbColumnLength = $dbColumn->getDataType()->getLength();
+            if($dbColumnLength != null) {
+                if($this->findInstanceImpactByLength($changeInput['length'],$dbColumnLength)) {
+                    return true;
+                }
+            }
+        }
+        if ($changeInput['scale'] !== null) {
+            $dbColumnScale = $dbColumn->getDataType()->getScale();
+            if($dbColumnScale != null) {
+                if($this->findInstanceImpactByScale($changeInput['scale'],$dbColumnScale)) {
+                    return true;
+                }
+            }
+        }
+        if ($changeInput['unique'] !== null) {
+            if($this->findInstanceImpactByUnique($changeInput['unique'], $this->isColumnUnique($frInput['tableName'],$frInput['columnName']))) {
+                return true;
+            }
+        }
+        if ($changeInput['nullable'] !== null) {
+            $dbColumnNullable = $dbColumn->isNullable();
+            if($this->findInstanceImpactByNullable($changeInput['nullable'],$dbColumnNullable)) {
+                return true;
+            }
+        }
+        if ($changeInput['min'] !== null) {
+            $dbColumnMin = $this->
+        }
+        if ($changeInput['max'] !== null) {
+
+        }
+    }
+
+    private function analysisAdding(array $changeInput, array $frInput): array
+    {
+
+    }
+
+    private function analysisDeleting(array $changeInput, array $frInput): array
+    {
+
+    }
+
+    private function findInstanceImpactByDataType(string $changeDataType, string $dbColumnDataType): bool {
+
+        $char = ['varchar', 'char'];
+        $unicodeChar = ['nvarchar', 'nchar'];
+        if(array_search($changeDataType,$char) && array_search($dbColumnDataType,$char) ){
+            return false;
+        }
+        if(array_search($changeDataType,$unicodeChar) && array_search($dbColumnDataType,$unicodeChar)) {
+            return false;
+        }
+        if($changeDataType == $dbColumnDataType) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    private function findInstanceImpactByLength(int $changeLength, int $dbColumnLength): bool {
+        return $changeLength >= $dbColumnLength ? false : true;
+    }
+
+    private function findInstanceImpactByScale(int $changeScale, int $dbColumnScale): bool {
+        return $changeScale >= $dbColumnScale ? false : true;
+    }
+
+    private function findInstanceImpactByNullable(bool $changeNullable, bool $dbColumnNullable): bool {
+        return ($changeNullable === false && $dbColumnNullable === true) ? true : false;
+    }
+
+    private function findInstanceImpactByUnique(bool $changeUnique, bool $dbColumnUnique): bool{
+        return ($changeUnique === true && $dbColumnUnique === false) ? true : false;
+    }
+
+    private function isColumnUnique(string $tableName, string $columnName): bool {
+        $uniqueConstraints = $this->dbTarget->getTableByName($tableName)->getAllUniqueConstraint();
+        foreach($uniqueConstraints as $uniqueConstraint){
+            foreach ($uniqueConstraint->getColumns() as $column) {
+                if($column == $columnName) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private function getMin(string $tableName, string $columnName) {
+        
     }
 
 }
